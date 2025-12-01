fff0c28e7b9a2da7206d7b6417c7b4ab31679abc
daijiaying1 <1773292109@qq.com>
2025-12-01 10:21:52 +0800

    feat

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@
 const develop = true
 // http://192.168.77.222:8000/api
 // https://www.liangstart.online/api	
-let host = 'https://www.crashcheck.net/api';
-if (develop) host = 'https://www.crashcheck.net/api'; 
+let host = 'https://www.liangstart.online/api';
+if (develop) host = 'https://www.liangstart.online/api'; 
 else '';
 console.log = develop ? console.log : () => {};
 console.info = develop ? console.info : () => {};
@@ -11,4 +11,5 @@ console.info = develop ? console.info : () => {};
 export {
 	host
 }
+// apk 签名：40fa8052a2655bbe8b1ec536e9dd276f  包名 crush.check
 
