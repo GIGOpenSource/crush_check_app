d7c79c0e47e9b2dd5c5018cc31cb0e5ab8f90576
daijiaying1 <1773292109@qq.com>
2025-12-02 10:54:52 +0800

    feat

--- i18n/locales/pt.js
+++ i18n/locales/pt.js
@@ -313,7 +313,7 @@ export default {
   login: {
     title: 'Login',
     appName: 'Crush Check',
-    appDesc: '——  Rápido de IA: TA é um Traidor? ——',
+   appDesc: '—— Teste Rápido de: TA é um Traidor? ——',
     wechatLogin: 'Login com Um Clique',
     loginFailedRetry: 'Falha no login, por favor tente novamente',
     agreeUserAgreement: 'Concordar com Acordo do Usuário',
